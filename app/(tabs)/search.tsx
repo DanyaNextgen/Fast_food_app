2d17b455a660bd87ab34fa9bcb6c9ec218dac52c
Danya-web <danilameretov20@gmail.com>
2025-07-10 19:03:30 +0500

    created Fast_food_app

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,11 @@
+import {View, Text, TouchableOpacity, Image} from "react-native"
+
+const Search = () => {
+    return (
+        <View>
+            <Text>Search</Text>
+        </View>
+    )
+}
+
+export default Search;
